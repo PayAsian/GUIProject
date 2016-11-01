2f8c9aceac6bb5cf23a10e804eea1e17727da06e
PayAsian <ptre8426@csddocs.org>
2016-11-01 12:51:05 -0600

    COLORS

--- src/gui/view/Panel.java
+++ src/gui/view/Panel.java
@@ -1,18 +1,24 @@
 package gui.view;
 
 
+import java.awt.Color;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import gui.controller.Controller;
 
 public class Panel extends JPanel
 {
 private Controller baseController;
+private JButton colorButton;
+private JButton randomButton;
 
 public Panel(Controller baseController)
 {
 	super();
 	this.baseController = baseController;
 	
+	this.colorButton = new JButton("colors");
+	this.randomButton = new JButton("random colors?");
 	setupPanel();
 	setupLayout();
 	setupListeners();
@@ -21,6 +27,9 @@ public Panel(Controller baseController)
 
 private void setupPanel()
 {
+	this.setBackground(Color.BLUE);
+	this.add(colorButton);
+	this.add(randomButton);
 	
 }
 
